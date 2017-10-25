413fec59cf2aa5f1723bd32e59eff6a38c466977
meiching <me@mgeo.de>
2017-10-25 09:02:13 +0200

    Examples

--- src/boot.sh
+++ src/boot.sh
@@ -3,16 +3,16 @@
 mdir=/etc/docker/modules
 updir=/etc/docker/up.d
 for mod in $(ls ${mdir}/*.sh); do
-  chmod a+x ${mdir}/$mod
-  echo " * Sourcing ${mdir}/$mod"
+  chmod a+x $mod
+  echo " * Sourcing $mod"
   . ${mdir}/$mod
 done
 #
 # INIT
 #
 for ups in $(ls ${updir}/*.sh); do
-  chmod a+x ${rp}/$updir
-  ${rp}/$updir
+  chmod a+x $updir
+  $updir
 done
 
 #
