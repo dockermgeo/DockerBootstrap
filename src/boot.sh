aa9ea260a1b12d9ab4c22059b93259cadcdaaf32
meiching <me@mgeo.de>
2017-11-14 17:14:03 +0100

    postup.d

--- src/boot.sh
+++ src/boot.sh
@@ -2,20 +2,34 @@
 
 mdir=/etc/docker/modules
 updir=/etc/docker/up.d
+postdir=/etc/docker/postup.d
+POST_WATING_TIME=${POST_WATING_TIME:-10};
 
 for mod in $(ls ${mdir}/*.sh); do
   echo " * Sourcing $mod"
   . $mod
 done
 #
-# INIT
+# INIT up.d
 #
 for ups in $(ls ${updir}/*.sh); do
-  echo " * Starting $ups"
+  echo " * Starting up.d $ups"
   $ups
 done
 
 #
-# Starting
+# Starting app
 #
-/app.sh
+nohup /app.sh &
+
+
+#
+# INIT postup.d
+#
+sleep ${POST_WATING_TIME};
+for pups in $(ls ${postdir}/*.sh); do
+  echo " * Starting POST-up.d $pups"
+  $pups
+done
+
+tail -f nohup.out
