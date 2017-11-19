6ab1e2f113840a22a1f4855a1a5edec96e10f814
meiching <me@mgeo.de>
2017-11-19 20:52:26 +0100

    [So 19 Nov 2017 20:52:26 CET] Makefile

--- src/boot.sh
+++ src/boot.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
+#
+# Variables
+#
 mdir=/etc/docker/modules
 updir=/etc/docker/up.d
 postdir=/etc/docker/postup.d
 POST_WATING_TIME=${POST_WATING_TIME:-10};
 
-for mod in $(ls ${mdir}/*.sh); do
+for mod in $(ls ${mdir}/*.sh 2> /dev/null); do
   echo " * Sourcing $mod"
   . $mod
 done
+
 #
 # INIT up.d
 #
-for ups in $(ls ${updir}/*.sh); do
+for ups in $(ls ${updir}/*.sh 2> /dev/null); do
   echo " * Starting up.d $ups"
   $ups
   if [ $? -gt 0 ]; then
@@ -25,11 +29,9 @@ done
 #
 /app.sh &
 
-
 #
 # Write PID-File to stdout
 #
-sleep 1
 pidfile=/tmp/pit
 touch ${pidfile}
 echo "pid=$(ps -ef|grep nginx  |grep off|awk '{ print $2 }')">>${pidfile}
@@ -38,7 +40,7 @@ echo "pid=$(ps -ef|grep nginx  |grep off|awk '{ print $2 }')">>${pidfile}
 # INIT postup.d
 #
 sleep ${POST_WATING_TIME};
-for pups in $(ls ${postdir}/*.sh); do
+for pups in $(ls ${postdir}/*.sh 2> /dev/null); do
   echo " * Starting POST-up.d $pups"
   $pups
 done
