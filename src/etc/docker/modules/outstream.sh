120ba717760f09a3be500886dbcbc4d1d182f3ac
meiching <me@mgeo.de>
2017-11-20 02:35:20 +0100

    [Mo 20 Nov 2017 02:35:20 CET] src

--- src/etc/docker/modules/outstream.sh
+++ src/etc/docker/modules/outstream.sh
@@ -7,9 +7,12 @@ runlevel=${LOG_LEVEL:-"INFO"}
 info () {
 	echo " [INFO] $1"
 }
+export -f info
+
 infolog () {
 	echo " [INFO] $1">>${pidfile}
 }
+export -f infolog
 
 # INFO
 debug () {
@@ -17,24 +20,30 @@ debug () {
 		echo " [DEBUG] $1"
 	fi
 }
-debug () {
+export -f debug
+debuglog () {
 	if [ "${runlevel}" = "DEBUG"]; then
 		echo " [DEBUG] $1">>${pidfile}
 	fi
 }
+export -f debuglog
 
 # WARNING
 warn () {
 	echo " [WARNING] $1"
 }
+export -f warn
 warnlog () {
 	echo " [WARNING] $1">>${pidfile}
 }
+export -f warnlog
 
 # ERROR
 error () {
 	echo " [ERROR] $1"
 }
+export -f error
 errorlog () {
 	echo " [ERROR] $1">>${pidfile}
 }
+export -f errorlog
