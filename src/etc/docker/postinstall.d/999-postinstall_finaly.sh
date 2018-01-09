d51c6f22fd895ea9142bf86178ef1543f5a99570
meiching <me@mgeo.de>
2018-01-09 22:58:12 +0100

    [Di  9 Jan 2018 22:58:12 CET] Makefile

--- src/etc/docker/postinstall.d/999-postinstall_finaly.sh
+++ src/etc/docker/postinstall.d/999-postinstall_finaly.sh
@@ -8,11 +8,12 @@ stepCur=0
 ## Set ACC
 infocounter "Setting global ACC's"
 chmod -v a+x /etc/docker/up.d/*
+chmod -v a+x /etc/docker/postup.d/*
+chmod -v a+x /etc/docker/postinstall.d/*
 chmod -v a+x /*.sh
 
 #
 ## Cleanup
 infocounter "Cleanup Global's"
-rm -Rvf /etc/docker/postinstall*
 rm -Rvf /tmp/Docker* >>/dev/null
 rm -Rvf /tmp/.* >>/dev/null
